8595de2add8826f00ec11393130930318ca17c49
unknown <majestic13@gmail.com>
2022-09-20 07:51:49 +0300

    database controller

---
+++ models/index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+require("./Car");
+require("./Accessory");
+const connectionString = "mongodb://localhost:27017/Cars";
+
+async function init() {
+  try {
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      autoIndex: false,
+    });
+
+    console.log("Database connection established...");
+
+    mongoose.connection.on("error", (err) => {
+      console.error("Database error");
+      console.error(err);
+    });
+  } catch (err) {
+    console.error("Error connecting to database");
+    process.exit(1);
+  }
+}
+
+module.exports = init;
