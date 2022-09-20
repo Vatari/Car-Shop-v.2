617e9c0b7a9af4418246e935d0912212300aee0a
unknown <majestic13@gmail.com>
2022-09-20 07:50:48 +0300

    added accessory model

---
+++ models/Accessory.js
@@ -0,0 +1,20 @@
+const {
+  Schema,
+  model,
+  Types: { ObjectId },
+} = require("mongoose");
+
+const accessorySchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, default: "" },
+  imageUrl: {
+    type: String,
+    default: "../static/images/no_image.jpeg",
+  },
+  price: { type: Number, min: 0 },
+  owner: { type: ObjectId, ref: "User" },
+});
+
+const Accessory = model("Accessory", accessorySchema);
+
+module.exports = Accessory;
